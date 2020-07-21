419ee21b353f07b07b4d44587cd1bb1ac5173fcc
bnkhek <bnk1@rice.edu>
2020-07-21 15:05:55 -0400

    Updates

---
+++ precache-manifest.6dcc7a32391a98c2574cc78b64417043.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a8a1249b762936be6d2a95fcb597f0",
+    "url": "/bnkheksite/index.html"
+  },
+  {
+    "revision": "12df2f743a0b293565bf",
+    "url": "/bnkheksite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6375de06f88cce803cd1",
+    "url": "/bnkheksite/static/js/2.98ad1bb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bnkheksite/static/js/2.98ad1bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12df2f743a0b293565bf",
+    "url": "/bnkheksite/static/js/main.2c5e2cdf.chunk.js"
+  },
+  {
+    "revision": "71d801b4b50263fb47e3",
+    "url": "/bnkheksite/static/js/runtime-main.05fcbfb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bnkheksite/static/media/logo.5d5d9eef.svg"
+  }
+]);
