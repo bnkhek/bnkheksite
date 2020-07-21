16970b65edb76d034f775750706b82dece7c2df1
bnkhek <bnk1@rice.edu>
2020-07-21 17:37:19 -0400

    Transition Main Page

--- src/Routes/App.js
+++ src/Routes/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
+import Slide from "react-reveal/Slide";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <body className="App-body">
         <h1 id="name">Brandon Khek.</h1>
         <Link to="/About">Go to About</Link>
-      </header>
+        <p>
+          <i class="arrow down"></i>
+        </p>
+
+        <Slide right>
+          <div className="page2">
+            <h2>As you can tell, I like making websites!</h2>
+            <p>
+              I actually made this one from scratch in React.js. Not bad for an
+              astrophysics major, eh?
+            </p>
+          </div>
+        </Slide>
+      </body>
     </div>
   );
 }
