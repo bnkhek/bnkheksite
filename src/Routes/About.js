fd894d144dc08568ad73cc226e8fcaf8746a3645
bnkrandon <bnkhek@gmail.com>
2020-10-03 17:38:52 -0500

    Lots of details

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -43,7 +43,41 @@ function About() {
           <p>(Outside of work!)</p>
         </div>
 
-        <p>I like to play tennis, chess, and skateboard.</p>
+        <p>
+          I like to play tennis, chess, and skateboard. (Work in progress
+          section...)
+        </p>
+
+        <h1>About the Site</h1>
+        <p className="contentDetail">
+          Since first grade I've always wanted to create my own website, though
+          as a 9 year old, finding out that website hosting wasn't free at the
+          time was heartbreaking. Also, making websites is hard! It wasn't until
+          recently that I developed the skills and found the resources to
+          execute such task.
+        </p>
+        <p className="contentDetail">
+          I'm currently using GitHub pages to host this site, though the biggest
+          by far challenge was the first step to building it. Over the summer
+          most of my learning was done in an already developed code base, so I
+          wasn't super well equipped to build one from the ground up. Especially
+          since I wanted to build the site using React.js instead of traditional
+          HTML/JS, things were somewhat complicated. I also experimented with
+          changing the domain name, using temporary free domains to verify that
+          I could get it to work, ultimately so that I could buy a domain in my
+          name and use it for this site once it's done.
+        </p>
+
+        <p className="contentDetail">
+          After getting it up and running, the next big task was getting various
+          subdirectories working. I ended up using React Hash Router for this
+          since for some reason I couldn't get React Router to work, thus you
+          see the # in the URL. But after accomplishing this, I was all set. All
+          that is left is to make the rest of the site good looking and
+          informational, and this is where things start falling into place! The
+          only thing I didn't do was make a UI/UX wireframe of the site... I'm
+          developing this on a day by day basis.
+        </p>
       </body>
     </div>
   );
--- src/Routes/App.js
+++ src/Routes/App.js
@@ -64,7 +64,7 @@ function App() {
               </div>
             </Slide>
             <Slide left>
-              <p>But enough about the site... who am I?</p>
+              <p>But enough about the site... what do I do?</p>
             </Slide>
           </div>
           <p>
@@ -76,6 +76,16 @@ function App() {
           <h1>
             <strong>Teaching</strong>
           </h1>
+
+          <p>
+            Coming into Rice, I was very uncertain with what I wanted to explore
+            in college. The guidance of countless students, mentors, and
+            professors enabled me to explore career paths that were
+            complementary to my interests. Being a resource to others means a
+            lot to me because of the invaluable self-discovery I can help
+            facilitate.
+          </p>
+
           <h2>RCEL Elite Tech Camp</h2>
           <div className="contentDetail">
             <img src={rcel} alt="RCEL Class Photo" width="90%" />
@@ -83,16 +93,18 @@ function App() {
               Over the summer of 2020 I had the pleasure of teaching 28 high
               school students machine learning in Python through RCEL - Rice
               Center for Engineering Leadership - technology camp. We used
-              anomaly detection algorithms to analyze meteorological data! This
-              is one of our classes trying to make a heart.
+              anomaly detection algorithms to analyze meteorological data!
+              Examples include the Isolation Forest and Local Outlier Factor
+              algorithms, aided with powerful libraries like SciKitLearn and
+              MatPlotLib. This is one of our classes trying to make a heart.
             </p>
           </div>
           <h2>COLL</h2>
           <p>
             COLL is a class code, short for COLLege course, which are
-            student-taught courses! After some training, Rice students can teach
-            a COLL course on anything they'd like, and I'm working on doing this
-            soon! Stay tuned for a syllabus :)
+            student-taught courses! After a semseter of training, Rice students
+            can teach a COLL course on anything they'd like, and I'm working on
+            doing this soon! Stay tuned for a syllabus.
           </p>
 
           <h2>Rice Splash</h2>
@@ -100,9 +112,10 @@ function App() {
           <p>
             There's a really cool organization on campus called Rice Splash, in
             which students at Rice can teach middle/high school students about
-            any topic they want. I'm going to be teaching this fall about
-            multivariable calculus. Ambitious indeed, but I'm confident the
-            material can be simplified enough and still have them learn.
+            any topic they want. I'm going to be teaching this fall about single
+            variable (and some multivariable) calculus. Ambitious, but I'm
+            confident the material can be simplified enough and still be
+            beneficial.
           </p>
 
           <p>
@@ -112,6 +125,7 @@ function App() {
 
         <div className="page page4">
           <h1>Research</h1>
+
           <h2>Gravitational Waves from Self-Ordering Scalar Fields</h2>
           <div className="contentDetail">
             <img
@@ -119,19 +133,25 @@ function App() {
               alt="Higgs Mexican Hat Potential"
               id="higgs"
             />
-
-            <p className="contentDetail">
-              I'm studying the gravitational wave energy density from
-              self-ordering scalar fields, particularly the Higgs field. Fancy
-              talk for saying that the Higgs field (associated with Higgs boson)
-              is inhomogeneous (isn't uniform) in space, and as it tries to
-              relax, or become homogeneous, it generates gravitational waves.
-              Woah! To the left is a picture of the{" "}
-              <a href="https://en.wikipedia.org/wiki/Spontaneous_symmetry_breaking">
-                Higgs Mexican Hat potential.{" "}
-              </a>
-              Hover your mouse over it!
-            </p>
+            <div>
+              <p className="contentDetail">
+                I'm studying the gravitational wave energy density from
+                self-ordering scalar fields, particularly the Higgs field. Fancy
+                talk for saying that the Higgs field (associated with Higgs
+                boson) is inhomogeneous (isn't uniform) in space, and as it
+                tries to relax, or become homogeneous, it generates
+                gravitational waves. Woah! To the left is a picture of the{" "}
+                <a href="https://en.wikipedia.org/wiki/Spontaneous_symmetry_breaking">
+                  Higgs Mexican Hat potential.{" "}
+                </a>
+              </p>
+              <p className="contentDetail">
+                I was ecstatic to have been able to do this research this
+                semester. I've finally gotten behind the pop science that
+                surrounds some of the terms mentioned here. To be able to study
+                a phenomenon so beyond us all is both mind-blowing and humbling.
+              </p>
+            </div>
           </div>
 
           <h2>Some Other Research</h2>
@@ -155,14 +175,24 @@ function App() {
             My co-chair and I won the SEDS (Students for the Exploration and
             Development of Space) USA Chapter bid for SpaceVision 2021, one of
             the largest student-run conferences in the world. We're working with
-            Space Center Houston (visitor center of the Johnson Space Center),
-            other SEDS Chapters, the national SEDS advisory board, and fellow
-            SEDS members to plan this event of 500+ people! The current 3 day
-            conference schedule includes topics ranging from intense
-            computational work to international space policy.
+            NASA Space Center Houston (visitor center of the Johnson Space
+            Center), other SEDS Chapters, the national SEDS advisory board, and
+            fellow SEDS members to plan this event of 500+ people! The 3 day
+            conference schedule includes topics ranging from the Artemis
+            missions to international space policy.
           </p>
+          {/*
           <h2>TEDxRice</h2>
-          <p>👀</p>
+          <p>
+            I was looking to give a talk illuminating equations in physics that
+            are overlooked but quite powerful in their implications and meaning.
+            I think pop science today goes a little too far with the qualitative
+            analysis of phenomena. While it's easily accessible this way, I felt
+            that the quantitative aspect of physics is simulatenously overlooked
+            and feared, even though there's a lot of weight to them. I'll try my
+            best to explain pop science physics more rigorously but still fun.
+          </p>
+          */}
           <p>
             <i class="arrow down"></i>
           </p>
@@ -177,10 +207,10 @@ function App() {
 
           <h2>RiceApps Open Source Accelerator</h2>
           <p>
-            Basically, RiceApps OSA was a 2 and a half-ish month program in
-            summer 2020. I was paired up with a recent Rice grad (also my
-            referral to FB, thanks Tam!) and two other students. More can be
-            found on my LinkedIn.
+            RiceApps OSA was a 2 and a half-ish month program in summer 2020. I
+            was paired up with a recent Rice grad (also my referral to FB,
+            thanks Tam!) and two other students. More can be found on my
+            LinkedIn.
           </p>
           <p>
             Bottom line, this experience showed me the{" "}
@@ -188,13 +218,13 @@ function App() {
             to develop this website after having absolutely no experience with
             web development. This was something I never got in my years of
             classroom coding experience. It was always Java, Pyton, or MATLAB,
-            but never front-end development! I wouldn't say OSA rekindled a
+            but never front-end development! OSA didn't necessarily rekindle a
             desire to code - I've always enjoyed it since my first class in high
             school. Rather, it gave me perspective to an area of programming
             that I've never been exposed to up until that point. Front-end
             development is a (very) recent discovery for me, and because of
             this, I want to see how much more I can do, perhaps as an intern
-            over the summer! I'm interested in seeing if such a hobby could turn
+            over the summer. I'm interested in seeing if such a hobby could turn
             into a career, and given my great (but short) experience with OSA,
             I'm very open minded and willing to see what else this path has to
             offer.
