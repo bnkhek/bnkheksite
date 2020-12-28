979b228b56a4fac6d6ee897273e9e5907c2bd2bb
bnkhek <bnk1@rice.edu>
2020-12-27 23:12:39 -0500

    Page Layout Change

--- src/Routes/App.js
+++ src/Routes/App.js
@@ -5,6 +5,7 @@ import Navbar from "../Components/Navbar.js";
 import rcel from "../Images/Heart.png";
 import { NavLink } from "react-router-dom";
 import Slide from "react-reveal/Slide";
+import Fade from "react-reveal/Fade";
 
 function App() {
   const [clicked, setClicked] = useState(false);
@@ -44,6 +45,10 @@ function App() {
           <p className="tagline">
             <strong>Astrophysics Student at Rice University</strong>
           </p>
+          <p className="tagline">
+            Undergraduate researcher interested in early and late universe
+            cosmology, astroparticle physics, and compact objects.
+          </p>
           <p>
             <i class="arrow down"></i>
           </p>
@@ -73,67 +78,82 @@ function App() {
         </div>
 
         <div className="page page3">
-          <h1>
-            <strong>Teaching</strong>
-          </h1>
-
-          <p>
-            Coming into Rice, I was very uncertain with what I wanted to explore
-            in college. The guidance of countless students, mentors, and
-            professors enabled me to explore career paths that were
-            complementary to my interests. Being a resource to others means a
-            lot to me because of the invaluable self-discovery I can help
-            facilitate.
-          </p>
-
-          <h2>RCEL Elite Tech Camp</h2>
-          <div className="contentDetail">
-            <img src={rcel} alt="RCEL Class Photo" width="90%" />
-            <p className="contentDetail">
-              Over the summer of 2020 I had the pleasure of teaching 28 high
-              school students machine learning in Python through RCEL - Rice
-              Center for Engineering Leadership - technology camp. We used
-              anomaly detection algorithms to analyze meteorological data!
-              Examples include the Isolation Forest and Local Outlier Factor
-              algorithms, aided with powerful libraries like SciKitLearn and
-              MatPlotLib. This is one of our classes trying to make a heart.
+          <div className="pageHeader">
+            <h1>
+              <strong>Teaching</strong>
+            </h1>
+            <hr className="divider"></hr>
+            <p>
+              Coming into Rice, I was very uncertain with what I wanted to
+              explore in college. The guidance of countless students, mentors,
+              and professors enabled me to explore career paths that were
+              complementary to my interests. Being a resource to others means a
+              lot to me because of the invaluable self-discovery I can help
+              facilitate.
             </p>
           </div>
-          <h2>COLL</h2>
-          <p>
-            COLL is a class code, short for COLLege course, which are
-            student-taught courses! After a semseter of training, Rice students
-            can teach a COLL course on anything they'd like, and I'm working on
-            doing this soon! Stay tuned for a syllabus.
-          </p>
 
-          <h2>Rice Splash</h2>
+          <div className="contentDetailDark">
+            <div>
+              <h2>RCEL Elite Tech Camp</h2>
+              <p>
+                Over the summer of 2020 I had the pleasure of teaching 28 high
+                school students machine learning in Python through RCEL - Rice
+                Center for Engineering Leadership - technology camp. We used
+                anomaly detection algorithms to analyze meteorological data!
+                Examples include the Isolation Forest and Local Outlier Factor
+                algorithms, aided with powerful libraries like SciKitLearn and
+                MatPlotLib. This is one of our classes trying to make a heart.
+              </p>
+            </div>
+            <img src={rcel} alt="RCEL Class Photo" width="90%" />
+          </div>
 
-          <p>
-            There's a really cool organization on campus called Rice Splash, in
-            which students at Rice can teach middle/high school students about
-            any topic they want. I'm going to be teaching this fall about single
-            variable (and some multivariable) calculus. Ambitious, but I'm
-            confident the material can be simplified enough and still be
-            beneficial.
-          </p>
+          <div className="contentDetailDark">
+            <h2>COLL</h2>
+            <p>
+              COLL is a class code, short for COLLege course, which are
+              student-taught courses! After a semseter of training, Rice
+              students can teach a COLL course on anything they'd like, and I'm
+              working on doing this soon! Stay tuned for a syllabus.
+            </p>
+          </div>
 
+          <div className="contentDetailDark">
+            <h2>Rice Splash</h2>
+
+            <p>
+              There's a really cool organization on campus called Rice Splash,
+              in which students at Rice can teach middle/high school students
+              about any topic they want. I'm going to be teaching this fall
+              about single variable (and some multivariable) calculus.
+              Ambitious, but I'm confident the material can be simplified enough
+              and still be beneficial.
+            </p>
+          </div>
           <p>
             <i class="arrow down"></i>
           </p>
         </div>
 
         <div className="page page4">
-          <h1>Research</h1>
-
-          <h2>Gravitational Waves from Self-Ordering Scalar Fields</h2>
-          <div className="contentDetail">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Mexican_hat_potential_polar.svg/1280px-Mexican_hat_potential_polar.svg.png"
-              alt="Higgs Mexican Hat Potential"
-              id="higgs"
-            />
+          <div className="pageHeader">
+            <h1>Research</h1>
+            <hr className="divider"></hr>
+            <p>
+              I found during my fall 2020 semester that research is an
+              invaluable component of my experience at Rice. It is an outlet to
+              explore my natural academic inclinations and further solidify my
+              confidence in pursuing postgraduate education. Ultimately, I love
+              to present my work to others that share the same joy in learning
+              astrophysics.
+            </p>
+          </div>
+          <div className="contentDetailDark">
             <div>
+              <h2 className="contentDetail">
+                Gravitational Waves from Self-Ordering Scalar Fields
+              </h2>
               <p className="contentDetail">
                 I'm studying the gravitational wave energy density from
                 self-ordering scalar fields, particularly the Higgs field. Fancy
@@ -152,35 +172,51 @@ function App() {
                 a phenomenon so beyond us all is both mind-blowing and humbling.
               </p>
             </div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Mexican_hat_potential_polar.svg/1280px-Mexican_hat_potential_polar.svg.png"
+              alt="Higgs Mexican Hat Potential"
+              id="higgs"
+            />
           </div>
-
-          <h2>Some Other Research</h2>
-          <p>
-            Particle physics is suprisingly computational. To put it into
-            perspective, the Large Hardon Collider collected about 5 petabytes
-            of data when looking for the Higgs Boson. We need complex algorithms
-            to filter out this noise and give us the good stuff! I'm going to
-            explore the programming side of physics after I'm done with my
-            current project.
-          </p>
+          <div className="contentDetailDark">
+            <h2>Some Other Research</h2>
+            <p>
+              Particle physics is suprisingly computational. To put it into
+              perspective, the Large Hardon Collider collected about 5 petabytes
+              of data when looking for the Higgs Boson. We need complex
+              algorithms to filter out this noise and give us the good stuff!
+              I'm going to explore the programming side of physics after I'm
+              done with my current project.
+            </p>
+          </div>
           <p>
             <i class="arrow down"></i>
           </p>
         </div>
 
         <div className="page page5">
-          <h1>Outreach</h1>
-          <h2>SpaceVision 2021</h2>
-          <p>
-            My co-chair and I won the SEDS (Students for the Exploration and
-            Development of Space) USA Chapter bid for SpaceVision 2021, one of
-            the largest student-run conferences in the world. We're working with
-            NASA Space Center Houston (visitor center of the Johnson Space
-            Center), other SEDS Chapters, the national SEDS advisory board, and
-            fellow SEDS members to plan this event of 500+ people! The 3 day
-            conference schedule includes topics ranging from the Artemis
-            missions to international space policy.
-          </p>
+          <div className="pageHeader">
+            <h1>Outreach</h1>
+            <hr className="divider"></hr>
+            <p>
+              I enjoy connecting with a wide audience and facilitating
+              opportunities to students. As a result, I have involved myself a
+              variety of outreach activities.
+            </p>
+          </div>
+          <div className="contentDetailDark">
+            <h2>SpaceVision 2021</h2>
+            <p>
+              My co-chair and I won the SEDS (Students for the Exploration and
+              Development of Space) USA Chapter bid for SpaceVision 2021, one of
+              the largest student-run conferences in the world. We're working
+              with NASA Space Center Houston (visitor center of the Johnson
+              Space Center), other SEDS Chapters, the national SEDS advisory
+              board, and fellow SEDS members to plan this event of 500+ people!
+              The 3 day conference schedule includes topics ranging from the
+              Artemis missions to international space policy.
+            </p>
+          </div>
           {/*
           <h2>TEDxRice</h2>
           <p>
